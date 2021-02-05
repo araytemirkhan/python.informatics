0bfc98253c3ea079b5bd9ee7cd32ce19ac5e9d11
araytemirkhan <77055497+araytemirkhan@users.noreply.github.com>
2021-02-06 04:36:57 +0600

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,17 @@
+#bn=b1*q^n-1
+#1
+#2 => 16
+#5
+
+#1)
+a = int(input())
+b = int(input())
+c = int(input())
+print(a * (b ** (c-1)))
+#2)
+print(int(input()) * (int(input()) ** (int(input())-1)))
+#3)
+b1 = int(input())
+q = int (input())
+n = int(input())
+print(b1 * q **(n - 1))
